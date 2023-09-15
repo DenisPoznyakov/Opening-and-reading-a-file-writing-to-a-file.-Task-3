c3b68ab72ac776c856a5e30f62c13126b063b95b
Denis Poznyakov <flint037@gmail.com>
2023-09-15 17:16:07 +0300

    commit

--- main.py
+++ main.py
@@ -1,35 +1,20 @@
-with open('1.txt', 'r', encoding='utf-8') as file:
-    lines_1 = len(file.readlines())
+import os
+dic_file = {}
+directory = r'C:\Users\Denis\PycharmProjects\Открытие и чтение файла, запись в файл. Задание 3\files'
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    with open(f, 'r', encoding='utf-8') as file:
+        count = sum(1 for line in file if line.rstrip('\n'))
+    with open(f, 'r', encoding='utf-8') as file:
+        list = file.read()
+        dic_file[filename] = count, list
 
-with open('2.txt', 'r', encoding='utf-8') as file:
-    lines_2 = len(file.readlines())
+dic_file_sort = sorted(dic_file.items(), key=lambda x: x[1])
 
-with open('3.txt', 'r', encoding='utf-8') as file:
-    lines_3 = len(file.readlines())
-
-files_lens = [lines_1, lines_2, lines_3]
-files_lens_sort = sorted(files_lens)
-
-with open('united.txt', 'a', encoding='utf-8') as file:
-    file.write(f'2.txt\n{str(files_lens_sort[0])}\n')
-
-with open('2.txt', 'r', encoding='utf-8') as first_file, open('united.txt', 'a', encoding='utf-8') as second_file:
-    for line in first_file:
-        second_file.write(line)
-    second_file.write('\n')
-
-with open('united.txt', 'a', encoding='utf-8') as file:
-    file.write(f'1.txt\n{str(files_lens_sort[1])}\n')
-
-with open('1.txt', 'r', encoding='utf-8') as first_file, open('united.txt', 'a', encoding='utf-8') as second_file:
-    for line in first_file:
-        second_file.write(line)
-    second_file.write('\n')
-
-with open('united.txt', 'a', encoding='utf-8') as file:
-    file.write(f'3.txt\n{str(files_lens_sort[2])}\n')
-
-with open('3.txt', 'r', encoding='utf-8') as first_file, open('united.txt', 'a', encoding='utf-8') as second_file:
-    for line in first_file:
-        second_file.write(line)
-    second_file.write('\n')
+for file_list in dic_file_sort:
+    with open('united.txt', 'a', encoding='utf-8') as file:
+        file.write(str(file_list[0]))
+        file.write('\n')
+        for i in file_list[1]:
+            file.write(str(i))
+            file.write('\n')
